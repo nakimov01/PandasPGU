f752fbd6584994e54b1718c33c7a1ef617fa23f8
nakimov01 <nikita2003akimov@bk.ru>
2023-02-18 08:56:05 +0300

    A1 done A2 done

--- main.py
+++ main.py
@@ -8,5 +8,25 @@ List.insert(0, Avg__Val)  # Добавление среднего значени
 print(List, "\n")
 
 N, M = 5, 5
-Matr = [[round(random.uniform(-99.999, 100), 2) for i in range(N)] for j in range(M)]  # Создание матрицы 5x5
-for Row in Matr: print(*Row)
+Matr = [[round(random.uniform(1, 5)) for i in range(N)] for j in range(M)]  # Создание матрицы 5x5
+
+res_sum = 0
+for i in range(len(Matr)):
+    for j in range(len(Matr[i])):
+        print(Matr[i][j], end = ' ')
+    print()
+for i in range(len(Matr)):
+    for j in range(len(Matr[i])):
+        if i == j:
+            res_sum += Matr[i][j]
+            print(Matr[i][j])
+print(res_sum)
+for i in range(len(Matr)):
+    for j in range(len(Matr[i])):
+        if j == (4-i):
+            print(Matr[i][j])
+            Matr[i][j] = 0
+for i in range(len(Matr)):
+    for j in range(len(Matr[i])):
+        print(Matr[i][j], end = ' ')
+    print()
